678de79addcb9e576378de278912e53f53fe7f85
Jakob <jped22@student.aau.dk>
2025-03-11 14:56:00 +0100

    Rigged Guy imported

---
+++ P6_Unity/Assets/Server.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+public class Server : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
