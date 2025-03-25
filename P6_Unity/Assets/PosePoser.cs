36f887a3889a7ce13675f86176b6a1ee0ff3a303
Jakob <jped22@student.aau.dk>
2025-03-25 15:03:26 +0100

    Posing der ikke helt virker
    Makker er helt messed up

---
+++ P6_Unity/Assets/PosePoser.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PosePoser : MonoBehaviour
+{
+    //GameObject rækkfølge i listen:  nose, neck, rShoulder, rElbow, rWrist, lShoulder, lElbow, lWrist, rHip, rKnee, rAnkle, rEye, lEye, rEar, lEar
+    public List<GameObject> poses = new List<GameObject>();
+
+    public List<float> rawXValues = new List<float>();
+    public List<float> xValues = new List<float>();
+    public List<float> rawYValues = new List<float>();
+    public List<float> yValues = new List<float>();
+    public List<float> depthValues = new List<float>();
+
+    public bool pose;
+    public float scalingValue;
+
+    void Start()
+    {
+        for(int i = 0; i < rawXValues.Count; i++)
+        {
+            xValues.Add(rawXValues[i]);
+            yValues.Add(rawYValues[i]);
+        }
+    }
+
+    void Update()
+    {
+        ChangePose();
+    }
+
+    public void ChangePose()
+    {
+        for (int i = 0; i < poses.Count; i++)
+        {
+            poses[i].transform.position = new Vector3(rawXValues[i] / scalingValue, -rawYValues[i] / scalingValue, 0);
+        }
+    }
+}
